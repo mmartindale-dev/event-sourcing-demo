f72003c8333c1195bb646049dae857367d6c40db
mmartindale-dev <269044154+mmartindale-dev@users.noreply.github.com>
2026-04-10 14:48:24 -0600

    fix - corrected syntax errors

--- src/domain/item.ts
+++ src/domain/item.ts
@@ -1,10 +1,10 @@
 export type Item = {
-    id: string, // UUID
-    label: string, // user-assigned label
-    delivered: boolean,
-    unpacked: boolean,
-    placed: boolean,
-    createdAt: string // ISO timestamp
+    id: string; // UUID
+    label: string; // user-assigned label
+    delivered: boolean;
+    unpacked: boolean;
+    placed: boolean;
+    createdAt: string; // ISO timestamp
     location?: {
         room: string; // room item is placed in
         shelf: boolean; // true = on a shelf, false = on the floor
